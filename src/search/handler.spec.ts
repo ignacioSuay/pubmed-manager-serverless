50b79f5b47f9205fa11b7f9057ec7f94a82dd3be
44087895 <ignacio.suay@gmail.com>
2018-06-02 15:42:35 +0100

    search for summary

--- src/search/handler.spec.ts
+++ src/search/handler.spec.ts
@@ -4,16 +4,26 @@ import axios from "axios";
 var MockAdapter = require('axios-mock-adapter');
 import 'mocha';
 const  searchResponse = require('../../test/resources/search.test.response.json');
+const  summaryResponse = require('../../test/resources/summary.response.json');
 
 
 describe('search publications', () => {
 
-    it('should return a publication', async function () {
-        var mock = new MockAdapter(axios);
-        mock.onGet().reply(200, searchResponse);
+    it('should return 20 publications', async function () {
 
+        //Given
+        var mock = new MockAdapter(axios);
+        mock.onGet(new RegExp('esearch')).reply(200, searchResponse);
+        mock.onGet(new RegExp('esummary')).reply(200, summaryResponse);
+
+        //When
         const result = await search(null, null);
+
+        //Then
         console.log("resultado " + JSON.stringify(result));
+        expect(result).to.not.be.null;
+        expect(result.statusCode).to.equal(200);
+        expect(result.body)
 
     });
 
