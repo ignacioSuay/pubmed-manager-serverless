978371ed3680e228a5ac5bb80aaa6644e8d576e7
44087895 <ignacio.suay@gmail.com>
2018-06-04 14:49:17 +0100

    update handler to take into account term, startPage and endPage

--- src/search/handler.ts
+++ src/search/handler.ts
@@ -1,25 +1,33 @@
 import axios from "axios";
 import {PubMedSearch, PubMedSummary, PubMedSummaryItem} from "../model/pubmed/pubmed.types";
-import {Publication, Author} from "../model/publication.type";
+import {Publication, Author, Response} from "../model/publication.type";
 
-interface Response2 {
-    statusCode: number;
-    body: string;
-}
+export const search = async (event) => {
+    console.log("running search");
 
-export const search = async (event, context) => {
+    if(!event.queryStringParameters.term || !event.queryStringParameters.startPage || !event.queryStringParameters.endPage ){
+        return {
+            statusCode: 500,
+            body: "Term or startPage or endPage cannot be empty",
+        };
+    }
 
-    const pubMedsearchPromise = await axios("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=cancer&usehistory=y&retmode=JSON");
+    const pubMedsearchPromise = await axios(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=${event.queryStringParameters.term}&usehistory=y&retmode=JSON`);
     const pubMedsearch = pubMedsearchPromise.data as PubMedSearch;
 
-    const pubMedSummaryPromise = await axios("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&query_key=1&WebEnv=NCID_1_270230973_130.14.18.34_9001_1527934798_111025544_0MetA0_S_MegaStore&version=2.0&retmode=JSON&retstart=1&retmax=20")
+    const pubMedSummaryPromise = await axios(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&query_key=${pubMedsearch.esearchresult.querykey}&WebEnv=${pubMedsearch.esearchresult.webenv}&version=2.0&retmode=JSON&retstart=${event.queryStringParameters.startPage}&retmax=${event.queryStringParameters.endPage}`)
     const pubMedsummary = pubMedSummaryPromise.data as PubMedSummary;
 
+    const pubs = buildPublications(pubMedsummary);
 
-    console.log("res: " + JSON.stringify(pubMedsummary.result["29857127"].uid));
-    console.log("res: " + JSON.stringify(pubMedsummary.result.length));
+    return {
+        statusCode: 200,
+        body: JSON.stringify(pubs),
+    };
+};
 
-    const pubs: Publication[] = Object.keys(pubMedsummary.result)
+function buildPublications(pubMedsummary) {
+    return  Object.keys(pubMedsummary.result)
         .map((key) => {
             return pubMedsummary.result[key]
         })
@@ -29,15 +37,7 @@ export const search = async (event, context) => {
         .map((pubMedItem) => {
             return transformPublication(pubMedItem);
         });
-
-
-    const response: Response2 = {
-        statusCode: 200,
-        body: JSON.stringify(pubs),
-    };
-
-    return response;
-};
+}
 
 function transformPublication(pub: PubMedSummaryItem): Publication {
 
