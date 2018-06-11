9d4c5662d718829c3660faf795026121e8afb87a
44087895 <ignacio.suay@gmail.com>
2018-06-11 10:51:39 +0100

    Simplified get details

--- src/details/handler.ts
+++ src/details/handler.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-import {ArticleId, PubMedSummary, PubMedSummaryItem} from "../model/pubmed/pubmed.types";
-import {Author, Publication, Response} from "../model/publication.type";
+import {Response} from "../model/publication.type";
 
 const convert = require('xml-js');
 
@@ -11,68 +10,22 @@ export const getPublicationDetails = async (event) => {
         return buildResponse(500, "Publication id is not present");
     }
 
-    const pubMedSummary = await getPubMedSummary(event);
-    const pub = await buildPublications(pubMedSummary);
+    const pub = await buildPublicationDetails(event.pathParameters.id);
 
     return buildResponse(200, JSON.stringify(pub));
 };
 
-async function getPubMedSummary(event) {
-    const pubMedSummaryPromise = await axios(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=${event.pathParameters.id}&version=2.0&retmode=JSON`);
-    return pubMedSummaryPromise.data as PubMedSummary;
-}
-
-function buildPublications(pubMedsummary) {
-    const pubMedItem = Object.keys(pubMedsummary.result)
-        .map((key) => pubMedsummary.result[key])
-        .find((pubMedItem) => pubMedItem.uid !== undefined);
-
-    return transformPublication(pubMedItem);
-}
-
-async function transformPublication(pub: PubMedSummaryItem): Promise<Publication> {
-
-    const authors: Author[] = pub.authors.map((aut) => {
-        return {name: aut.name, authType: aut.authtype}
-    });
-
-    const fetch = await axios(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=${pub.uid}&rettype=Abstract`);
+async function buildPublicationDetails(id) {
+    const fetch = await axios(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=${id}&rettype=Abstract`);
     const fetchResult = convert.xml2js(fetch.data, {compact: true});
-
-    console.log(JSON.stringify(fetchResult));
 
     let abstractText;
     if (fetchResult.PubmedArticleSet.PubmedArticle.MedlineCitation.Article.Abstract.AbstractText._text) {
         abstractText = fetchResult.PubmedArticleSet.PubmedArticle.MedlineCitation.Article.Abstract.AbstractText._text;
     }
     return {
-        uid: pub.uid,
-        title: pub.title,
-        pubdate: pub.pubdate,
-        epubdate: pub.epubdate,
-        source: pub.source,
-        authors: authors,
-        lastauthor: pub.lastauthor,
-        volumen: pub.volumen,
-        issue: pub.issue,
-        pages: pub.pages,
-        lang: pub.lang,
-        issn: pub.issn,
-        pubtype: pub.pubtype,
-        pubmedId: getArticleId(pub.articleids, "pubmed"),
-        doi: getArticleId(pub.articleids, "doi"),
         abstract: abstractText
     }
-}
-
-function getArticleId(articleIds: ArticleId[], type: string): string {
-
-    const articleId = articleIds.find((articleId) => articleId.idtype === type);
-    if (articleId) {
-        return articleId.value;
-    }
-    return "";
-
 }
 
 function buildResponse(status, body): Response {
