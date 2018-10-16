b4466cc2b79b20185c5ff07ecac15ba18edff38d
Ignacio Suay <ignacio.suay@gmail.com>
2018-10-16 15:32:19 +0000

    fix imports

--- src/postFavorite/handler.spec.ts
+++ src/postFavorite/handler.spec.ts
@@ -1,15 +1,9 @@
 import {expect} from "chai";
-import {DataMapper} from "@aws/dynamodb-data-mapper";
-import {mock} from "ts-mockito";
 import {postFavorite} from "./handler";
 import {FavoriteItem} from "../model/entity/Favorite";
-import {id} from "aws-sdk/clients/datapipeline";
-import {Publication} from "../model/publication.type";
 const  event = require('../../test/resources/inputSamples/saveFavorite.json');
 const DynamoDB = require('aws-sdk/clients/dynamodb');
 const client = new DynamoDB({region: 'eu-west-1'});
-const mapper = new DataMapper({client});
-
 
 describe('save favorites', () => {
 
