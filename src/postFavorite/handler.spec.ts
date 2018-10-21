9d4f0d58a0801d7e248be4bcf255b88e7aeb9095
ignacio suay <ignacio.suay@gmail.com>
2018-10-21 19:04:52 +0100

    fix post fav test

--- src/postFavorite/handler.spec.ts
+++ src/postFavorite/handler.spec.ts
@@ -7,14 +7,14 @@ const client = new DynamoDB({region: 'eu-west-1'});
 
 describe('save favorites', () => {
 
-    before(() => {
+    before((done) => {
         const params = {TableName:"Favorite", Key: {"id" :{"S": "testId"}}};
         client.deleteItem(params, function(err, data){
             if(err)
                 console.log("error " + err);
             if(data)
-                console.log(data);
-
+                console.log("Deleting testId");
+            done()
         })
     });
 
@@ -25,7 +25,14 @@ describe('save favorites', () => {
 
         //then
         console.log(result);
-        const favoritesaved: FavoriteItem = JSON.parse(result.body);
-        expect(favoritesaved.publications.length).to.equal(1);
+        const favoriteSaved: FavoriteItem = JSON.parse(result.body);
+        expect(favoriteSaved.id).to.eq("testId");
+        expect(favoriteSaved.publications.length).to.equal(1);
+        expect(favoriteSaved.publications[0].authors.length).to.equal(8);
+        expect(favoriteSaved.publications[0].epubdate).to.equal("2015 Jan 10");
+        expect(favoriteSaved.publications[0].doi).to.equal("10.1016/j.ejim.2014.12.010");
+        expect(favoriteSaved.publications[0].title).to.equal("Cardiovascular profile in critically ill elderly medical patients: prevalence, mortality and length of stay.");
+        expect(favoriteSaved.publications[0].uid).to.equal("25582073");
+        expect(favoriteSaved.publications[0].pubtype[0]).to.equal("Journal Article");
     })
 });
